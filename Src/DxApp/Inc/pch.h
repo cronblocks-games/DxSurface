b3340f732f4af78b3686962f36ae4ae46eb8208c
usama <29387130+usa-m@users.noreply.github.com>
2024-02-18 16:19:39 +0500

    adding props and minimal app

---
+++ Src/DxApp/Inc/pch.h
@@ -0,0 +1,8 @@
+#ifndef _DXAPP_PCH_H_
+#define _DXAPP_PCH_H_
+
+
+#include <Windows.h>
+
+
+#endif
---
+++ Src/DxApp/Src/WinMain.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "DxSurface.h"
+
+int WINAPI WinMain(
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  PSTR pCmdLine,
+  int nCmdShow)
+{
+  return 0;
+}
