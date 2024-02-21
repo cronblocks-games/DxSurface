2b257326221406f75814ab706faa9261978bd2be
usama <29387130+usa-m@users.noreply.github.com>
2024-02-21 17:05:27 +0500

    adding version information macros

--- Src/DxSurface/Inc/DxsMacros.h
+++ Src/DxSurface/Inc/DxsMacros.h
@@ -3,7 +3,9 @@
 
 
 
-//-
+#define DXSURFACE_VERSION                        0x00'00'00'00ul
+#define DXSURFACE_VERSION_STRING                 "0.0.0.0"
+#define DXSURFACE_VERSION_RELEASE_DATE_STRING    "FEB/21/2024"
 
 
 
