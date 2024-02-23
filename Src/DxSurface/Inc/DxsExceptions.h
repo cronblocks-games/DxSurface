db59de53b822376098c2cd6aefe5b30380a32f19
usama <29387130+usa-m@users.noreply.github.com>
2024-02-23 19:26:10 +0500

    adding helper macro DXSURFACE_ENCLOSE_THROW to catch exceptions for single defined call

--- Src/DxSurface/Inc/DxsExceptions.h
+++ Src/DxSurface/Inc/DxsExceptions.h
@@ -30,6 +30,16 @@ namespace CB::DxSurface {
 }
 
 #define DXSURFACE_THROW(msg) {throw CB::DxSurface::Exception(__FILE__,__LINE__,msg);}
+#define DXSURFACE_ENCLOSE_THROW(call) {                                   \
+            try {                                                         \
+              call;                                                       \
+            } catch (Exception& ex) {                                     \
+              MessageBox(nullptr, ex.what(), "Error - DxSurface", 0);     \
+            } catch (std::exception& ex) {                                \
+              MessageBox(nullptr, ex.what(), "Error", 0);                 \
+            } catch (...) {                                               \
+              MessageBox(nullptr, "Unknown error occurred.", "Error", 0); \
+            }}
 
 
 
