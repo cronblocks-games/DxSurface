3db2ed10b1cdd3323fc27b1e1fc216896929fd85
usama <29387130+usa-m@users.noreply.github.com>
2024-02-25 19:25:54 +0500

    appending _UC or _MB with version to indicate unicode or multibyte compilation

--- Src/DxSurface/Inc/DxsMacros.h
+++ Src/DxSurface/Inc/DxsMacros.h
@@ -11,6 +11,16 @@
 #  endif
 #endif
 
+#if defined(_UNICODE) || defined(UNICODE)
+#  define DXSURFACE_VERSION_SIFFIX  "_UC"
+#else
+#  define DXSURFACE_VERSION_SIFFIX  "_MB"
+#endif
+
+#define DXSURFACE_VERSION                        0x00'00'00'00ul
+#define DXSURFACE_VERSION_STRING                 DxsT("0.0.0.0" DXSURFACE_VERSION_SIFFIX)
+#define DXSURFACE_VERSION_RELEASE_DATE_STRING    DxsT("FEB/21/2024")
+
 
 
 #endif
