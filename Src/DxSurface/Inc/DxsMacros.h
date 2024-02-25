fcd0cefb50179293cb481d12fbf0c888de892a37
usama <29387130+usa-m@users.noreply.github.com>
2024-02-25 19:25:11 +0500

    adding macro DxsT for converting string literal for unicode and multibyte usage

--- Src/DxSurface/Inc/DxsMacros.h
+++ Src/DxSurface/Inc/DxsMacros.h
@@ -3,15 +3,11 @@
 
 
 
-#define DXSURFACE_VERSION                        0x00'00'00'00ul
-#define DXSURFACE_VERSION_STRING                 "0.0.0.0"
-#define DXSURFACE_VERSION_RELEASE_DATE_STRING    "FEB/21/2024"
-
-#ifndef T
+#ifndef DxsT
 #  if defined(_UNICODE) || defined(UNICODE)
-#    define T(str_literal) L##str_literal
+#    define DxsT(str_literal) L##str_literal
 #  else
-#    define T(str_literal) str_literal
+#    define DxsT(str_literal) str_literal
 #  endif
 #endif
 
