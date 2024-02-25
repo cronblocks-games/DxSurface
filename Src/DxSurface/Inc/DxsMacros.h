94d37cfff8e25bcc00fbac19877337150a0a8c86
usama <29387130+usa-m@users.noreply.github.com>
2024-02-25 18:44:22 +0500

    adding macro for T - unicode and multibyte prefixing

--- Src/DxSurface/Inc/DxsMacros.h
+++ Src/DxSurface/Inc/DxsMacros.h
@@ -7,6 +7,14 @@
 #define DXSURFACE_VERSION_STRING                 "0.0.0.0"
 #define DXSURFACE_VERSION_RELEASE_DATE_STRING    "FEB/21/2024"
 
+#ifndef T
+#  if defined(_UNICODE) || defined(UNICODE)
+#    define T(str_literal) L##str_literal
+#  else
+#    define T(str_literal) str_literal
+#  endif
+#endif
+
 
 
 #endif
