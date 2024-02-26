e3e29995232142cc6f3faa39c20ca9a2dd058b23
usama <29387130+usa-m@users.noreply.github.com>
2024-02-26 20:04:58 +0500

    using the default operators for window creation options

--- Src/DxSurface/Inc/DxsTypes.h
+++ Src/DxSurface/Inc/DxsTypes.h
@@ -36,81 +36,13 @@ namespace CB::DxSurface {
   struct WindowRectangle
   {
     int x = 0, y = 0, w = 0, h = 0;
-
-    WindowRectangle(int _x = 0, int _y = 0, int _w = 0, int _h = 0)
-    {
-      x = _x;
-      y = _y;
-      w = _w;
-      h = _h;
-    }
-
-    WindowRectangle& operator=(const WindowRectangle& other) noexcept
-    {
-      x = other.x;
-      y = other.y;
-      w = other.w;
-      h = other.h;
-
-      return *this;
-    }
-    WindowRectangle& operator=(WindowRectangle&& other) noexcept
-    {
-      x = other.x;
-      y = other.y;
-      w = other.w;
-      h = other.h;
-
-      other.x = 0;
-      other.y = 0;
-      other.w = 0;
-      other.h = 0;
-
-      return *this;
-    }
   };
-
   struct WindowCreationOptions
   {
     bool isPrimary;
     bool isDebugEnabled = false;
     TString title;
     WindowRectangle rect;
-
-    WindowCreationOptions()
-    {
-      isPrimary = false;
-      isDebugEnabled = true;
-      title = DxsT("DxSurface");
-      rect = WindowRectangle(10, 10, 250, 250);
-    }
-    WindowCreationOptions(const WindowCreationOptions& options)
-    {
-      *this = options;
-    }
-
-    WindowCreationOptions& operator=(const WindowCreationOptions& other) noexcept
-    {
-      isPrimary = other.isPrimary;
-      isDebugEnabled = other.isDebugEnabled;
-      title = other.title;
-      rect = other.rect;
-
-      return *this;
-    }
-    WindowCreationOptions& operator=(WindowCreationOptions&& other) noexcept
-    {
-      isPrimary = other.isPrimary;
-      other.isPrimary = false;
-
-      isDebugEnabled = other.isDebugEnabled;
-      other.isDebugEnabled = false;
-
-      title = std::move(other.title);
-      rect = std::move(other.rect);
-
-      return *this;
-    }
   };
 
   enum class WindowCreationState
