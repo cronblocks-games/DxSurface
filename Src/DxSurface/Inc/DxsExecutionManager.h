78cfe2b4dc0d0b53854469bf35dc90e22b34f9bf
usama <29387130+usa-m@users.noreply.github.com>
2024-02-28 15:03:36 +0500

    updating the obj to generalize the concepts

--- Src/DxSurface/Inc/DxsExecutionManager.h
+++ Src/DxSurface/Inc/DxsExecutionManager.h
@@ -10,41 +10,63 @@
 namespace CB::DxSurface {
 
   template<class T>
-  class ExecutionManagerBase {
+  class ExecutableObjectBase {
   public:
-    ExecutionManagerBase(TString name, unsigned int maxRefreshRateHz)
+    ExecutableObjectBase(TString name, unsigned int maxRefreshRateHz)
     {
       m_sName = name;
       m_uiMaxRefreshRateHz = maxRefreshRateHz;
+      m_pThread.reset();
+      m_eState = ExecutionState::NONE;
+      m_eCommand = ExecutionCommand::Exit;
+      m_eExitReason = ExecutionExitReason::Normal;
+      m_eExitMessage = DxsT("");
     }
-    ExecutionManagerBase(const ExecutionManagerBase& o)
+    ExecutableObjectBase(const ExecutableObjectBase& o)
     {
       if (this == &o) return;
       *this = o;
     }
-    ExecutionManagerBase(ExecutionManagerBase&& o) noexcept
+    ExecutableObjectBase(ExecutableObjectBase&& o) noexcept
     {
       if (this == &o) return;
       *this = std::move(o);
     }
-
-    ExecutionManagerBase& operator=(const ExecutionManagerBase& o)
+    ExecutableObjectBase& operator=(const ExecutableObjectBase& o)
+    {
+      if (this == &o) return *this;
+      
+      m_sName = o.name;
+      m_uiMaxRefreshRateHz = o.maxRefreshRateHz;
+      m_pThread.reset(); // Thread cannot be copied, each object has its own thread
+      m_eState = ExecutionState::NONE;
+      m_eCommand = ExecutionCommand::Exit;
+      m_eExitReason = ExecutionExitReason::Normal;
+      m_eExitMessage = DxsT("");
+    }
+    ExecutableObjectBase& operator=(ExecutableObjectBase&& o) noexcept
     {
       if (this == &o) return *this;
       //- TODO
     }
-    ExecutionManagerBase& operator=(ExecutionManagerBase&& o) noexcept
+
+    virtual ~ExecutableObjectBase()
     {
-      if (this == &o) return *this;
-      //- TODO
+      m_eCommand = ExecutionCommand::Exit;
+      if (m_pThread) m_pThread->join();
+      m_pThread.reset();
     }
 
-    virtual ~ExecutionManagerBase()
+    void operator()()
     {
-      //- TODO
+      Run();
     }
-
-    void Run();
+    void Run()
+    {
+      if (m_pThread) return;
+      m_eCommand = ExecutionCommand::Run;
+      m_pThread = std::make_shared<Thread>(ExecutableObjectBase::ExecutionThread, this);
+    }
     ExecutionState GetState() const
     {
       return m_eState;
@@ -71,6 +93,14 @@ namespace CB::DxSurface {
     {
       m_eCommand = ExecutionCommand::Exit;
     }
+    ExecutionExitReason GetExitReason() const
+    {
+      return m_eExitReason;
+    }
+    const TString& GetExitMessage() const
+    {
+      return m_eExitMessage;
+    }
     void WaitForExit()
     {
       if (m_pThread)
@@ -82,7 +112,7 @@ namespace CB::DxSurface {
 
   protected:
     virtual void OnExecutionStateInit() = 0;
-    virtual void OnExecutionStateRunning() = 0;
+    virtual void OnExecutionStateRunning(double deltaSec) = 0;
     virtual void OnExecutionStateExiting() = 0;
     virtual void OnExecutionStateChanged(ExecutionState last, ExecutionState next) = 0;
 
@@ -92,13 +122,61 @@ namespace CB::DxSurface {
     Ptr<Thread> m_pThread;
     volatile ExecutionState m_eState;
     volatile ExecutionCommand m_eCommand;
+    volatile ExecutionExitReason m_eExitReason;
+    TString m_eExitMessage;
 
-    static void ExecutionThread()
+    void SetExecutionState(ExecutionState s)
     {
-      while (m_eCommand != ExecutionCommand::Exit)
+      if (m_eState != s)
       {
-        ;
+        OnExecutionStateChanged(m_eState, s)
+        m_eState = s;
       }
+    }
+    void ProcessExecutionState(double deltaSec)
+    {
+      //
+    }
+
+    static void ExecutionThread(ExecutableObjectBase* const b)
+    {
+      b->m_eExitReason = ExecutionExitReason::Normal;
+      b->m_eExitMessage = DxsT("");
+
+      try
+      {
+        b->SetExecutionState(ExecutionState::Init);
+        b->ProcessExecutionState(0.0);
+
+        while (b->m_eCommand != ExecutionCommand::Exit)
+        {
+          //- Setting the state if changed
+          switch (b->m_eCommand)
+          {
+          case ExecutionCommand::Run:
+            b->SetExecutionState(ExecutionState::Running);
+            break;
+          case ExecutionCommand::Pause:
+            b->SetExecutionState(ExecutionState::Paused);
+            break;
+          }
+          //- Processing the state as needed
+          b->ProcessExecutionState(0.0);
+        }
+      }
+      catch (const Exception& ex)
+      {
+        b->m_eExitReason = ExecutionExitReason::Exception;
+        b->m_eExitMessage = ex.Message();
+      }
+      catch (...)
+      {
+        b->m_eExitReason = ExecutionExitReason::Exception;
+        b->m_eExitMessage = DxsT("Unknown error occurred");
+      }
+
+      b->SetExecutionState(ExecutionState::Exitted);
+      b->ProcessExecutionState(0.0);
     }
   };
 
