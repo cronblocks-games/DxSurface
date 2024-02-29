8137c76f92515c5b9734434b98b8a974842ed1b9
usama <29387130+usa-m@users.noreply.github.com>
2024-02-29 17:37:52 +0500

    adding windows messages presenter class for internal debug usage

---
+++ Src/DxSurface/Src/InternalInc/DxsWinMessages.hpp
@@ -0,0 +1,14 @@
+#ifndef _DXSURFACE_DXSWINMESSAGES_H_
+#define _DXSURFACE_DXSWINMESSAGES_H_
+
+
+namespace CB::DxSurface {
+
+  class WindowsMessages {
+
+  };
+
+} //- namespace CB::DxSurface
+
+
+#endif
