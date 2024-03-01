dc47e65907a535bf7136640d1e22ab5d43441688
usama <29387130+usa-m@users.noreply.github.com>
2024-03-01 18:25:25 +0500

    adding basic key status functionality

--- Src/DxSurface/Inc/DxsKeyboard.hpp
+++ Src/DxSurface/Inc/DxsKeyboard.hpp
@@ -4,12 +4,19 @@
 
 
 #include "DxsHelpers.hpp"
+#include <bitset>
 
 namespace CB::DxSurface {
 
   class Keyboard {
+  public:
+    enum class KeyStatus KeyStatus(enum class KeyCode);
 
   private:
+    std::bitset<(1 << (8 * sizeof(enum class KeyCode)))> m_bKeyPressedStatus;
+
+    void SetKeyStatus(enum class KeyCode, enum class KeyStatus);
+
     friend class CB::DxSurface::Window;
   };
 
--- Src/DxSurface/Src/DxsKeyboard.cpp
+++ Src/DxSurface/Src/DxsKeyboard.cpp
@@ -1,2 +1,26 @@
 #include "pch.hpp"
 #include "DxsKeyboard.hpp"
+
+
+using namespace CB::DxSurface;
+
+
+enum class KeyStatus Keyboard::KeyStatus(enum class KeyCode k)
+{
+  if (m_bKeyPressedStatus[(unsigned char)k])
+    return KeyStatus::Pressed;
+
+  return KeyStatus::Released;
+}
+
+void Keyboard::SetKeyStatus(enum class KeyCode k, enum class KeyStatus s)
+{
+  if (s == KeyStatus::Pressed)
+    m_bKeyPressedStatus[(unsigned char)k] = true;
+  else
+    m_bKeyPressedStatus[(unsigned char)k] = false;
+}
+
+
+
+
