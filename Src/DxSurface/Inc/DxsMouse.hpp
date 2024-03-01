6f6afb3f0cf5a5473b2a3ddf800ca107a279fc2e
usama <29387130+usa-m@users.noreply.github.com>
2024-03-01 20:23:43 +0500

    adding getting and setting of mouse button presses

--- Src/DxSurface/Inc/DxsMouse.hpp
+++ Src/DxSurface/Inc/DxsMouse.hpp
@@ -10,13 +10,17 @@ namespace CB::DxSurface {
   class Mouse {
   public:
     Mouse();
+
     POINT GetPosition() const noexcept;
+    KeyStatus GetButtonStatus(MouseButton) const noexcept;
 
   private:
     bool m_bIsValid;
     POINT m_ptPosition;
+    KeyStatus m_eLeftButtonStatus, m_eRightButtonStatus, m_eMiddleButtonStatus;
 
     void SetPosition(POINT) noexcept;
+    void SetButtonStatus(MouseButton, KeyStatus) noexcept;
 
     friend class CB::DxSurface::Window;
   };
--- Src/DxSurface/Src/DxsMouse.cpp
+++ Src/DxSurface/Src/DxsMouse.cpp
@@ -7,14 +7,36 @@ Mouse::Mouse()
 {
   m_bIsValid = false;
   m_ptPosition = { 0 };
+  m_eLeftButtonStatus = m_eRightButtonStatus = m_eMiddleButtonStatus = KeyStatus::Released;
 }
 
-POINT CB::DxSurface::Mouse::GetPosition() const noexcept
+POINT Mouse::GetPosition() const noexcept
 {
   return m_ptPosition;
+}
+
+KeyStatus Mouse::GetButtonStatus(MouseButton b) const noexcept
+{
+  switch (b)
+  {
+  case MouseButton::Left: return m_eLeftButtonStatus;
+  case MouseButton::Right: return m_eRightButtonStatus;
+  case MouseButton::Middle: return m_eMiddleButtonStatus;
+  }
+  return KeyStatus::Released;
 }
 
 void Mouse::SetPosition(POINT p) noexcept
 {
   m_ptPosition = p;
+}
+
+void Mouse::SetButtonStatus(MouseButton b, KeyStatus s) noexcept
+{
+  switch (b)
+  {
+  case MouseButton::Left: m_eLeftButtonStatus = s; break;
+  case MouseButton::Right: m_eRightButtonStatus = s; break;
+  case MouseButton::Middle: m_eMiddleButtonStatus = s; break;
+  }
 }
