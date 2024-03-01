69b4af84efa5d45f247d7fcd42d19aa37b659078
usama <29387130+usa-m@users.noreply.github.com>
2024-03-01 20:23:03 +0500

    adding MouseButton enum for left right and middle buttons

--- Src/DxSurface/Inc/DxsTypes.hpp
+++ Src/DxSurface/Inc/DxsTypes.hpp
@@ -464,6 +464,10 @@ namespace CB::DxSurface {
     PA1 = VK_PA1,                                    // 0xFD - PA1 key
     OEM_Clear = VK_OEM_CLEAR,                        // 0xFE - Clear key
   };
+  enum class MouseButton
+  {
+    Left, Right, Middle
+  };
   enum class KeyStatus
   {
     Pressed, Released
