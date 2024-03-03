5c3cff00ab9ce90d2dfa5b9f9e7bbcd52d928d35
usama <29387130+usa-m@users.noreply.github.com>
2024-03-03 17:41:19 +0500

    updating hInstance according to loading type

--- Src/DxSurface/Src/DxsResources.cpp
+++ Src/DxSurface/Src/DxsResources.cpp
@@ -1,2 +1,16 @@
 #include "pch.hpp"
 #include "DxsResources.hpp"
+
+using namespace std;
+using namespace CB::DxSurface;
+
+WinImageResource::WinImageResource(
+  unsigned int resourceId,
+  UINT type, int cx, int cy, UINT flags,
+  ResourceType r, Source s, HINSTANCE hInstance)
+{
+  if (s == Source::ExeEmbedded && hInstance == nullptr)
+  {
+    hInstance = GetModuleHandle(nullptr);
+  }
+}
