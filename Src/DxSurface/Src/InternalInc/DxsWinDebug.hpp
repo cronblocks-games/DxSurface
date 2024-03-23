22135e88389a911d458ab84e23f586953efb5103
usama <29387130+usa-m@users.noreply.github.com>
2024-03-23 14:07:22 +0500

    adding comments

--- Src/DxSurface/Src/InternalInc/DxsWinDebug.hpp
+++ Src/DxSurface/Src/InternalInc/DxsWinDebug.hpp
@@ -6,13 +6,21 @@
 
 namespace CB::DxSurface {
 
+  // Allows printing specifically formatted messages to the attached
+  // debug console.
   class WinDebug {
   public:
+    // Prints given Windows API 'msg' like WM_* as string to the attached debug console.
     static void PrintWindowsMessage(UINT msg, WPARAM wParam, LPARAM lParam);
+    // Prints a member name of enum KeyCode as string and the status as provided to the attached debug console.
     static void PrintKeyStatus(KeyCode, KeyStatus);
+    // Prints provided char to the attached debug console as if it is received from keyboard.
     static void PrintCharReceived(TChar);
+    // Prints provided point as mouse position within client region to the attached debug console.
     static void PrintMouseClientPosition(POINT);
+    // Prints provided mouse button, its status and point to the attached debug console.
     static void PrintMouseClientButtonStatus(MouseButton, KeyStatus, POINT);
+    // Prints provided mouse wheel status and point to the attached debug console.
     static void PrintMouseWheelStatus(short, POINT);
   };
 
