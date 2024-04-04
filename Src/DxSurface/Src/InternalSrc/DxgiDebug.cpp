ed75a864968495ab26746f4f187eb784931a192b
usama <29387130+usa-m@users.noreply.github.com>
2024-04-05 01:14:26 +0500

    loading library func from the dll

--- Src/DxSurface/Src/InternalSrc/DxgiDebug.cpp
+++ Src/DxSurface/Src/InternalSrc/DxgiDebug.cpp
@@ -5,6 +5,10 @@
 
 using namespace CB::DxSurface;
 
+
+using IfcFunc = HRESULT(WINAPI*)(REFIID, void**);
+
+
 DxgiDebug::DxgiDebug(bool isDebuggingEnabled)
   : m_bIsDebuggingEnabled(isDebuggingEnabled)
 {
@@ -12,6 +16,26 @@ DxgiDebug::DxgiDebug(bool isDebuggingEnabled)
 
   //- No need to load library when debugging is not enabled
   if (!m_bIsDebuggingEnabled) return;
+
+  //- Let's load the library
+	const HMODULE lib = LoadLibraryEx(DxsT("dxgidebug.dll"), nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+	if (lib == nullptr)
+	{
+		DxsThrowWindows(DxsT("Cannot load the DXGI debug library"));
+	}
+
+	const IfcFunc ifcFunc = reinterpret_cast<IfcFunc>(GetProcAddress(lib, "DXGIGetDebugInterface"));
+
+	if (ifcFunc == nullptr)
+	{
+		DxsThrowWindows(DxsT("Cannot locate DXGI debug interface function from the loaded library"));
+	}
+
+	HRESULT hr = ifcFunc(__uuidof(DxDXGIInfoQueue), &m_pInfoQueue);
+	if (hr != S_OK)
+	{
+		DxsThrowGraphicsHr(DxsT("Cannot get DXGI Info Queue"), hr);
+	}
 }
 
 void DxgiDebug::Mark() noexcept
