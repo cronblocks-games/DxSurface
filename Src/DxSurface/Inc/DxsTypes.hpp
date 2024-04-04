244eb0db3442366fee73e49b9e61509c159b6fd2
usama <29387130+usa-m@users.noreply.github.com>
2024-04-04 06:05:32 +0500

    adding DxFeatureLevel alias

--- Src/DxSurface/Inc/DxsTypes.hpp
+++ Src/DxSurface/Inc/DxsTypes.hpp
@@ -42,6 +42,7 @@ namespace CB::DxSurface {
   using DxDeviceContext = ID3D11DeviceContext;
   using DxRenderTargetView = ID3D11RenderTargetView;
   using DxResource = ID3D11Resource;
+  using DxFeatureLevel = D3D_FEATURE_LEVEL;
 
   using Thread = std::thread;
   using Mutex = std::mutex;
