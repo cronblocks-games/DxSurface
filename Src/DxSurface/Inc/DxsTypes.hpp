6bac21383092bf68719a06a9e8a444786542f4fa
usama <29387130+usa-m@users.noreply.github.com>
2024-04-13 08:40:49 +0500

    adding DxPixelShader as a type alias for pixel shader

--- Src/DxSurface/Inc/DxsTypes.hpp
+++ Src/DxSurface/Inc/DxsTypes.hpp
@@ -41,6 +41,7 @@ namespace CB::DxSurface {
   using DxSwapChain = IDXGISwapChain;
   using DxDeviceContext = ID3D11DeviceContext;
   using DxVertexShader = ID3D11VertexShader;
+  using DxPixelShader = ID3D11PixelShader;
   using DxRenderTargetView = ID3D11RenderTargetView;
   using DxResource = ID3D11Resource;
   using DxBlob = ID3DBlob;
