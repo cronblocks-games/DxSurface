ab1ab4e0a9d72f333c9c94224386c7bf679894ba
usama <29387130+usa-m@users.noreply.github.com>
2024-04-21 07:44:22 +0500

    formatting

--- Src/DxSurface/Src/InternalInc/Macros.hpp
+++ Src/DxSurface/Src/InternalInc/Macros.hpp
@@ -11,9 +11,9 @@
 /* Graphics calls & debugging                                 */
 /* ---------------------------------------------------------- */
 #if defined(DxsDebugBuild) && DxsGraphicsDebugEnabled == DxsTRUE
-//-------------------------------
-//- Debug version(s)
-//-------------------------------
+   //-------------------------------
+   //- Debug version(s)
+   //-----------------------------
 #  define DxDeviceCreationFlags D3D11_CREATE_DEVICE_DEBUG
 #  define DxCall(func_call,ignored_fail_msg) {                                \
             m_pDebugIface->Mark();                                            \
@@ -21,18 +21,16 @@
             if (hr != S_OK) {                                                 \
               DxsThrowGraphicsHr(m_pDebugIface->GetMessages().c_str(), hr);   \
             }} /* Should already have "DxgiDebugInterface *m_pDebugIface" defined in scope. */
-
 #else
-//-------------------------------
-//- Release version(s)
-//-------------------------------
+   //-------------------------------
+   //- Release version(s)
+   //-----------------------------
 #  define DxDeviceCreationFlags 0
 #  define DxCall(func_call,fail_msg) {   \
             HRESULT hr = func_call;      \
             if (hr != S_OK) {            \
               DxsThrowGraphicsHr(fail_msg DxsT(". Switch to debug build having 'DxsGraphicsDebugEnabled' set to 'DxsTRUE' in DxsSettings.hpp to get detailed information."), hr); \
             }}
-
 #endif
 
 /* ---------------------------------------------------------- */
@@ -53,6 +51,12 @@
                DxsLine("  return float4(1.0, 1.0, 1.0, 1.0);") \
                DxsLine("}")
 #endif
+
+
+
+
+
+
 
 
 #endif
